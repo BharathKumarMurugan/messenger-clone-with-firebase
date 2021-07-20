df6482f827879a1063ede9e4c2a30d9a56b18ce3
Bharath Kumar <bharathkumarmurugan2016@gmail.com>
2021-07-20 12:24:07 +0530

    Add files via upload

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,1 +1,16 @@
+import firebase from 'firebase';
+require('dotenv').config();
 
+const firebaseApp = firebase.initializeApp({
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
+});
+
+const db = firebaseApp.firestore();
+
+export default db;
