ca55e3707727a771db90e9b0d1036c0a292d8d5d
Bharath Kumar <bharathkumarmurugan2016@gmail.com>
2021-07-20 12:23:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import MessageBox from './components/MessageBox';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Messenger clone</h1>
+      <MessageBox />
+    </div>
+  );
+}
+
+export default App;
